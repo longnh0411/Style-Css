254f7f2f2ba5b451525a856fb0514de5e48efb19
Nguyễn Hoàng Long <longnh041120@gmail.com>
2024-07-08 14:58:39 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import UserInput from "./components/UserInput";
-import Result from "./components/Result";
-function App() {
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
 
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  const isValid = userInput.duration > 0 ? true : false;
-
-  function onChangeValue(inputName, value) {
-    setUserInput((curentValue) => {
-      return {
-        ...curentValue,
-        [inputName]: +value,
-      };
-    });
-  }
-
+export default function App() {
   return (
     <>
       <Header />
-      <UserInput userInput={userInput} onChangeValue={onChangeValue}/>
-      {isValid === false && <p className="center">{"Duration must be" + " > " + "0"}</p>}
-      {isValid === true && <Result userInput={userInput}/>}
+      <main>
+        <AuthInputs />
+      </main>
     </>
   );
 }
-
-export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import logo from "../assets/investment-calculator-logo.png";
+import logo from '../assets/logo.png';
 
 export default function Header() {
-    return(
-        <header id="header">
-            <img src={logo} alt=""/>
-            <h1>Investment Calculator</h1>
-        </header>
-    )
+  return (
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </header>
+  );
 }
